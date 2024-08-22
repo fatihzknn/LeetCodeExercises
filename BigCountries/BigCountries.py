6b9de4d3aa6bb6d6b4cb8732fc81487d1a219d51
fatihzknn <fatihhanozkn@gmail.com>
2024-08-22 20:07:10 +0300

    Files Updated

--- BigCountries/BigCountries.py
+++ BigCountries/BigCountries.py
@@ -5,3 +5,4 @@ def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     filtered_df = world[(world["area"] >= 3000000) | (world["population"] >= 25000000)]
 
     return filtered_df[["name","population","area"]]
+
